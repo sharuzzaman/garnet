77b70f68a48f718ce90667d6a75d69bf95027912
github-action-benchmark <github@users.noreply.github.com>
2024-12-13 18:29:18 +0000

    add Lua.LuaScripts (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for ca0222960f0145302eb1fc9896f680f9aff5a4d1

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734114556164,
+  "lastUpdate": 1734114558194,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Benchmark": [
@@ -7398,6 +7398,54 @@ window.BENCHMARK_DATA = {
             "value": 2928.858034133911,
             "unit": "ns",
             "range": "± 20.787703311031482"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "badrishc@microsoft.com",
+            "name": "Badrish Chandramouli",
+            "username": "badrishc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ca0222960f0145302eb1fc9896f680f9aff5a4d1",
+          "message": "Streaming snapshot checkpoint in Tsavorite (#824)\n\n* Mild refactor in preparation\r\n\r\n* updates\r\n\r\n* updates\r\n\r\n* updates\r\n\r\n* fix tests to not cover steaming snapshot as it is not a traditional checkpoint\r\n\r\n* add unit test\r\n\r\n* fix tests\r\n\r\n* fix test\r\n\r\n* updates\r\n\r\n* Support maxAddress during liveness checks\r\n\r\n* improvements to api\r\n\r\n* Add SetVersion API\r\n\r\n* more checks\r\n\r\n* add comment\r\n\r\n* add comments",
+          "timestamp": "2024-12-13T10:25:49-08:00",
+          "tree_id": "917be4f308af8da9ec7782faa5069a09f2b873c5",
+          "url": "https://github.com/microsoft/garnet/commit/ca0222960f0145302eb1fc9896f680f9aff5a4d1"
+        },
+        "date": 1734114556627,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script1(Params: None)",
+            "value": 188.91168311926035,
+            "unit": "ns",
+            "range": "± 1.1533494805322169"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script2(Params: None)",
+            "value": 323.5754240036011,
+            "unit": "ns",
+            "range": "± 2.6684226704152962"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script3(Params: None)",
+            "value": 596.7557873408,
+            "unit": "ns",
+            "range": "± 5.067161219452393"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script4(Params: None)",
+            "value": 2887.178291593279,
+            "unit": "ns",
+            "range": "± 28.770134276187093"
           }
         ]
       }
