497541b9bc6c008670685eb2381d038598cb41c3
github-action-benchmark <github@users.noreply.github.com>
2024-12-18 19:03:43 +0000

    add Lua.LuaScripts (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 100c7d90caa8357d75a9f0c1e99fc21f14a31e39

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1734548614084,
+  "lastUpdate": 1734548623076,
   "repoUrl": "https://github.com/microsoft/garnet",
   "entries": {
     "Cluster.ClusterMigrate (ubuntu-latest  net8.0 Release)": [
@@ -92,6 +92,56 @@ window.BENCHMARK_DATA = {
             "value": 1709.9032917022705,
             "unit": "ns",
             "range": "± 9.736436918112632"
+          }
+        ]
+      }
+    ],
+    "Lua.LuaScripts (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "100c7d90caa8357d75a9f0c1e99fc21f14a31e39",
+          "message": "Fixed so all BDN are based on Bytes (had one using KB). Updated Operations.ObjectOperations expected values to be in bytes. Also, noticed Operations.CustomOperations was accidentally removed from BDN tests so put that back. (#887)",
+          "timestamp": "2024-12-18T11:00:06-08:00",
+          "tree_id": "e9c3d6924c8d99786f0419adfd3d73d9710abab7",
+          "url": "https://github.com/microsoft/garnet/commit/100c7d90caa8357d75a9f0c1e99fc21f14a31e39"
+        },
+        "date": 1734548620970,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script1(Params: None)",
+            "value": 179.03324958256312,
+            "unit": "ns",
+            "range": "± 0.828352181888693"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script2(Params: None)",
+            "value": 326.11962039130077,
+            "unit": "ns",
+            "range": "± 2.1255854493965756"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script3(Params: None)",
+            "value": 601.9498613993327,
+            "unit": "ns",
+            "range": "± 5.550078922058537"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script4(Params: None)",
+            "value": 2993.6411012502817,
+            "unit": "ns",
+            "range": "± 19.438797247994145"
           }
         ]
       }
